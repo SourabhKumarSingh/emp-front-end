2e7ab163d75d23fa8e4e208732e525ad5ad9b4ce
SourabhKumarSingh <92620348+SourabhKumarSingh@users.noreply.github.com>
2023-03-19 17:33:07 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit {
+  employees: any[] = [];
+
+  employee={
+    id: 0,
+    name: ''
+  }
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  getEmployees() {
+    this.http.get('http://localhost:3000').subscribe(
+      {
+        next: (res: any) => {
+          this.employees = res;
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  addEmployee() {
+    this.http.post('http://localhost:3000', this.employee).subscribe(
+      {
+        next: (res: any) => {
+          this.getEmployees();
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
+  deleteEmployee(id: any) {
+    this.http.delete('http://localhost:3000/' + id).subscribe(
+      {
+        next: (res: any) => {
+          this.getEmployees();
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      }
+    );
+  }
+
 }
